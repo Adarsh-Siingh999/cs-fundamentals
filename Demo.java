973cb9318e8ceedc17d3b942733d6c4f67b284b8
Adarsh <singhadarshkr836@gmail.com>
2025-08-27 17:01:55 +0530

    New Code

---
+++ Demo.java
@@ -0,0 +1,22 @@
+class Demo{
+    public static void main(String[] args) {
+     int arr [][] = new int [7][10];
+
+     int random = (int)(Math.random()*100);
+     for(int i=0 ; i<3 ; i++)
+     {
+        for(int j =0 ; j<4 ; j++){
+            arr[i][j]= random;
+        }
+     }
+     
+    for(int i=0 ; i<3 ; i++)
+    {
+        for(int j =0 ; j<4 ; j++)
+        {
+          System.out.print(arr[i][j]+ " ");
+        } System.out.println();
+    } 
+
+    }
+} 
